cd0b00f138984ee25986d5a67d38077763346011
Juergen Hock <juergen.hock@jurihock.de>
2020-12-02 22:40:43 +0100

    Refactor require.js initialization

--- bandbase/assets/js/bootstrap/bootstrap.js
+++ bandbase/assets/js/bootstrap/bootstrap.js
@@ -9,3 +9,11 @@ require.config(
         'bootstrap': '/assets/js/bootstrap/js/bootstrap.bundle.min',
     }
 });
+
+require(['jquery', 'bootstrap'], function($)
+{
+    $(function()
+    {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+});
