0ebdd55804a01eeba04efd100d7370e70d2170d6
Juergen Hock <juergen.hock@jurihock.de>
2020-12-31 00:02:41 +0100

    Expose default PostgreSQL port 5432

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 
-: ${PGDATA:=/bandbase/postgresql}
+: ${PGDATA:=/bandbase/postgres}
 : ${PGDATABASE:="bandbase"}
 : ${PGUSER:="bandbase"}
 
 if [ ! -d "$PGDATA" ] || [ -z "$(ls -A "$PGDATA")" ]
 then
 
-	if [ -d "$PGDATA" ]
-	then
-		chown -R postgres:postgres $PGDATA
-	fi
+  mkdir -p $PGDATA && \
+  chown -R postgres:postgres $PGDATA
 
 	gosu postgres initdb $PGDATA
 
-	echo "create database $PGDATABASE;" | gosu postgres postgres --single -jE
-	echo "create user $PGUSER;" | gosu postgres postgres --single -jE
-	echo "grant all privileges on database $PGDATABASE to $PGUSER;" | gosu postgres postgres --single -jE
+	echo "create database $PGDATABASE;" | \
+	gosu postgres postgres --single -jE
+
+	echo "create user $PGUSER;" | \
+	gosu postgres postgres --single -jE
+
+	echo "grant all privileges on database $PGDATABASE to $PGUSER;" | \
+	gosu postgres postgres --single -jE
 
 fi
 
 if [ ! -d "/run/postgresql" ]
 then
-	mkdir /run/postgresql
+	mkdir /run/postgresql && \
 	chown postgres:postgres /run/postgresql
 fi
 
