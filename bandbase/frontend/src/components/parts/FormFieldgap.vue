216133028a9d9a9dd3bc81685a5da1cc48ccae27
Juergen Hock <juergen.hock@jurihock.de>
2021-01-05 23:40:15 +0100

    Add ability to insert custom form components

---
+++ bandbase/frontend/src/components/parts/FormFieldgap.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-bind:class="[offset, width]">
+    <slot/>
+  </div>
+</template>
+
+<script>
+function LayoutData(value) {
+  var values = value.split(',');
+  console.assert([1, 2].indexOf(values.length) >= 0);
+  if (values.length < 2) values.unshift(0);
+  this.offset = values[0] ? 'offset-sm-' + values[0] : '';
+  this.width = values[1] ? 'col-sm-' + values[1] : '';
+}
+
+export default {
+  name: 'FormFieldgap',
+  components: {},
+  data: function() {
+    return new LayoutData(this.layout)
+  },
+  props: {
+    layout: {
+      type: String,
+      required: false,
+      default: '2,10'
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
