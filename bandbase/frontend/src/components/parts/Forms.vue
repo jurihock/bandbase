a36bafc539172d02f7e3944ce30ab42e8b15e129
Juergen Hock <juergen.hock@jurihock.de>
2021-01-08 15:46:20 +0100

    Collect all form component imports in a single file

---
+++ bandbase/frontend/src/components/parts/Forms.vue
@@ -0,0 +1,27 @@
+<script>
+import Form from '@/components/parts/Form.vue';
+import FormDate from '@/components/parts/FormDate.vue';
+import FormFieldgap from '@/components/parts/FormFieldgap.vue';
+import FormFieldrow from '@/components/parts/FormFieldrow.vue';
+import FormFieldset from '@/components/parts/FormFieldset.vue';
+import FormGeopoint from '@/components/parts/FormGeopoint.vue';
+import FormList from '@/components/parts/FormList.vue';
+import FormTab from '@/components/parts/FormTab.vue';
+import FormText from '@/components/parts/FormText.vue';
+import FormTextArea from '@/components/parts/FormTextArea.vue';
+import FormTime from '@/components/parts/FormTime.vue';
+
+export default {
+  Form,
+  FormDate,
+  FormFieldgap,
+  FormFieldrow,
+  FormFieldset,
+  FormGeopoint,
+  FormList,
+  FormTab,
+  FormText,
+  FormTextArea,
+  FormTime
+};
+</script>
