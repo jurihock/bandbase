fda21f45661d194e9265b4fc57cc920877bed941
Juergen Hock <juergen.hock@jurihock.de>
2021-01-09 17:39:41 +0100

    Ensure the toast message is a string

--- bandbase/frontend/src/toaster.js
+++ bandbase/frontend/src/toaster.js
@@ -1,19 +1,32 @@
 import mitt from 'mitt';
+import _toString from 'lodash/toString';
 
 const events = mitt();
 
 const toaster = {
+  popup: function() {
+    window.scrollTo(0, 0);
+    return this;
+  },
+  subject: function(subject, message) {
+    events.emit(_toString(subject), _toString(message));
+    return this;
+  },
   toast: function(message) {
-    events.emit('toast', message);
+    events.emit(_toString(message));
+    return this;
   },
   success: function(message) {
-    events.emit('success', message);
+    events.emit('success', _toString(message));
+    return this;
   },
   warning: function(message) {
-    events.emit('warning', message);
+    events.emit('warning', _toString(message));
+    return this;
   },
   error: function(message) {
-    events.emit('error', message);
+    events.emit('error', _toString(message));
+    return this;
   },
   on: function(event, callback) {
     events.on(event, callback);
